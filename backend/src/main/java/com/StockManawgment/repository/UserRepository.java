a92f0d3d67c5cc287b330a5e7c7afb167cf8a982
Pabindu <pabinduamodya@gmail.com>
2026-03-02 23:41:17 +0530

    creating user repo

---
+++ backend/src/main/java/com/StockManawgment/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.StockManawgment.Stock_Managment.repository;
+
+import com.StockManawgment.Stock_Managment.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
